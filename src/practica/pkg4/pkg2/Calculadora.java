41050e20afc2717c60deec6d32e72019b7b25501
HectorCS <hcalsan2102@g.educaand.es>
2024-03-01 10:12:28 +0100

    Clase calculadora con la función suma

---
+++ src/practica/pkg4/pkg2/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.pkg4.pkg2;
+
+/**
+ *
+ * @author hecto
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 13;
+
+        System.out.println("La suma de a y b es: " + sumar(a, b));
+
+    }
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+}
