a4e1e695523ae532c51de19400fd9cefc04944ce
HectorCS <hcalsan2102@g.educaand.es>
2024-03-01 10:47:22 +0100

    Se le añade a la clase Calculadora funcion multiplicar

--- src/practica/pkg4/pkg2/Calculadora.java
+++ src/practica/pkg4/pkg2/Calculadora.java
@@ -19,11 +19,16 @@ public class Calculadora {
         int b = 13;
 
         System.out.println("La suma de a y b es: " + sumar(a, b));
+        System.out.println("La multiplicacion de a y b es: " + multiplicar(a, b));
 
     }
 
     public static int sumar(int a, int b) {
         return a + b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
     }
 
 }
